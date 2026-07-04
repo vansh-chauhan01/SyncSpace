a1a0e9c8ac4e6b7c17d72ea198a93a92c0bd9fa9
vansh-chauhan01 <98vansh98@gmail.com>
2026-07-04 23:55:25 +0530

    completer socket controller

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -27,15 +27,65 @@ export const connectToSocket = (server) =>{
                     io.to(socket.id).emit("chat-message", message['data'], message['sender'] , message['socket-id-sender']);
                 })
             }
+        })
+
+
+        // we are sending id because so that reciever knows who is sending the message
+        // user1->server->user2
+        socket.on("signal" , (id , message)=>{
+            io.to(id).emit("signal" , message , socket.id); // send message to the user with the id of the user that is joining the call
+        })
 
 
 
+        socket.on("chat-message" , (data , sender) =>{
+            let currRoom = undefined;
+            for(let room in connections){
+                if(connections[room].includes(socket.id)){
+                    currRoom = room;
+                    break;
+                }
+            }
+
+            if(currRoom){
+                if(!messages[currRoom]) messages[currRoom] = [];
+                messages[currRoom].push({'sender' : sender , 'data' : data , 'socket-id-sender' : socket.id});
+
+                connections[currRoom].forEach(id =>{
+                    io.to(id).emit("chat-message", data , sender , socket.id);
+                })
+            }
+        })
+
+        socket.on("disconnect" , ()=>{
+            let currRoom = undefined;
+
+            for(let room in connections){
+                if(connections[room].includes(socket.id)){
+                    currRoom = room;
+                    break;
+                }
+            }
+
+            if(!currRoom) return;
+
+            if(currRoom){
+                connections[currRoom] = connections[currRoom].filter((id)=>{
+                    return id !== socket.id;
+                })
+            }
+
+            connections[currRoom].forEach(id =>{
+                io.to(id).emit("user-left" , socket.id);
+            }) 
+
+            if(connections[currRoom].length === 0){
+                delete connections[currRoom];
+            }
         })
 
 
     })
-
-
 
     return io;
 }
